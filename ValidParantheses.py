af350f3208641cc72a1b2cf1ed7bed3edc6c1e03
Ian Fund <IFund@EnersysCorp.local>
2020-05-28 09:35:36 -0500

    Validate Strings added

---
+++ ValidParantheses.py
@@ -0,0 +1,48 @@
+
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+# Second solution implements a map and cleans up code from solution 1
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+        
+        if len(s) == 1:
+            return False
+        stack = ["Z"]
+        for i in range (0,len(s)):
+            if s[i] == ')' and stack[len(stack)-1] == '(':
+                stack.pop(len(stack) - 1)
+                continue
+            #    s.pop(i)
+            if s[i] == '}' and stack[len(stack)-1] == '{':
+                stack.pop(len(stack) - 1)
+              #  s.pop(i)
+                continue
+            if s[i] == ']' and stack[len(stack)-1] == '[':
+                stack.pop(len(stack) - 1)
+                continue
+              #  s.pop(i)
+            else:
+                stack.append(s[i])
+        if stack[len(stack)-1] == "Z":
+            return True
+        return False
+            
+            
+      def isValid2(self, s: str) -> bool:
+        if len(s) == 0:
+            return True
+        symbols = {")":"(","}":"{","]":"[" }
+        stack = []
+        for char in s:
+            if char in symbols:
+                if len(stack) == 0:
+                    return False
+                if stack.pop() != symbols[char]:
+                    return False
+            else:
+                stack.append(char)
+        if len(stack) == 0:
+            return True
+        return False
+            
