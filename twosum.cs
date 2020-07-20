0fd9a8728a6120e926cf56f1d48da7adc86e5872
Ian <ianfund117@gmail.com>
2020-07-19 21:52:11 -0500

    One Pass Two Sum

--- twosum.cs
+++ twosum.cs
@@ -4,18 +4,14 @@ public class Solution {
         Dictionary<int,int> compliment = new Dictionary<int,int>();
         for(int i = 0; i <nums.Length; i++){
             int value = target - nums[i];
-            compliment.TryAdd(value,i);
-        }
-        for (int i= 0; i < nums.Length; i++){
             if (compliment.ContainsKey(nums[i])){
-                if (i == compliment[nums[i]])
-                    continue;
                 result[0] = i;
                 result[1] = compliment[nums[i]];
                 return result;
             }
-                
+            compliment.TryAdd(value,i);
         }
+   
         return result;
     }
 }
